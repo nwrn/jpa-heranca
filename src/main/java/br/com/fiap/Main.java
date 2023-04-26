7337985c022dda41f76d45b1ac80a446dd86c6ab
nwrn <gmnp98@gmail.com>
2023-04-26 12:03:43 -0300

    Mapeamento de relacionamento e herança em Conta, Implementamos a classe Conta, ContaCorrente e ContaBancaria

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,5 +1,7 @@
 package br.com.fiap;
 
+import br.com.fiap.model.conta.Agencia;
+import br.com.fiap.model.conta.ContaCorrente;
 import br.com.fiap.pessoa.model.PF;
 import br.com.fiap.pessoa.model.PJ;
 import br.com.fiap.pessoa.model.Pessoa;
@@ -15,7 +17,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("maria-db");
+        EntityManagerFactory factory = Persistence
+                .createEntityManagerFactory("maria-db");
         EntityManager manager = factory.createEntityManager();
 
         PF bene = new PF();
@@ -29,6 +32,15 @@ public class Main {
         supermercado.setInscricaoEstadual("2132465464");
         supermercado.setNascimento(LocalDate.now().minusYears(5));
         supermercado.setNome("Super Mercados Benezinho");
+
+        var agencia = new Agencia();
+        agencia.setNumero(1);
+
+        var ccGustavo = new ContaCorrente();
+        ccGustavo.setSaldo(56078);
+        ccGustavo.setLimite(99999);
+        ccGustavo.setAgencia(agencia);
+        ccGustavo.addTitular(bene);
 
         manager.getTransaction().begin();
         List<Pessoa> pessoas = Arrays.asList(bene, supermercado);
